95d34243021af6533dd3f7cf666e0c380537b39f
bappy-ahashan <bappyahashan@gmail.com>
2022-11-01 05:20:56 +0600

    adding name and flags

--- src/App.js
+++ src/App.js
@@ -14,11 +14,37 @@ function LoadCountries() {
   useEffect(() => {
     fetch('https://restcountries.com/v3.1/all')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setCountries(data))
   }, [])
+
+
   return (
     <div>
       <h1>Visiting Every Country in the world</h1>
+      <p>Available Countries : {countries.length}</p>
+      <div className="countries-container">
+        {
+          countries.map(country => <DisplayCountries
+            country={country}
+          ></DisplayCountries>)
+        }
+      </div>
+
+    </div>
+  )
+}
+function DisplayCountries(props) {
+  const { name, population, region, area, flags } = props.country;
+  return (
+
+    <div className='countries'>
+
+      <h2>Name: {name.common}</h2>
+      <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Region: {region}</p>
+      <p>Area: {area}</p>
+      <img src="flag={props.country.flag.png}" alt="" />
     </div>
   )
 }
